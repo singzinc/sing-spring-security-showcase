412e0aed03e539acc6ce60aaf47498974171af7b
sing <sing.zinc.hk@gmail.com>
2016-08-01 13:44:53 +0800

    update the test

--- sing-spring-security/src/main/java/com/singplayground/showcase/controller/JsonController.java
+++ sing-spring-security/src/main/java/com/singplayground/showcase/controller/JsonController.java
@@ -1,5 +1,36 @@
 package com.singplayground.showcase.controller;
 
+import java.util.HashMap;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "/json")
 public class JsonController {
+
+	@RequestMapping(value = "public/test1", method = RequestMethod.GET)
+	public @ResponseBody String publicJson1Controller() throws Exception {
+		System.out.println("return string");
+		return "test";
+	}
+
+	@RequestMapping(value = "public/test2", method = RequestMethod.GET)
+	public @ResponseBody HashMap publicJson2Controller() throws Exception {
+		System.out.println("return json for public");
+		HashMap hm = new HashMap();
+		hm.put("name", "john");
+		return hm;
+	}
+
+	@RequestMapping(value = "private/test", method = RequestMethod.GET)
+	public @ResponseBody HashMap privateJsonController() throws Exception {
+		System.out.println("return json for private");
+		HashMap hm = new HashMap();
+		hm.put("name", "may");
+		return hm;
+	}
 
 }
